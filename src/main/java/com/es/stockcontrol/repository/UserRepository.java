e13835d2ae026d30c06b777b1de2001f9f6c5a0b
Abraham Rodriguez <arm.developer00@gmail.com>
2024-10-18 19:37:29 +0200

    create

---
+++ src/main/java/com/es/stockcontrol/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.es.stockcontrol.repository;
+
+import com.es.stockcontrol.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class UserRepository {
+
+
+
+}
+

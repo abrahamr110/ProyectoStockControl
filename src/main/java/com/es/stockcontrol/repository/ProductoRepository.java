9e635c96557f388a5fc4a275c2b510e1ed38dc42
Abraham Rodriguez <arm.developer00@gmail.com>
2024-10-22 21:00:45 +0200

    Update ProductoRepository.java

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -7,23 +7,4 @@ import java.util.List;
 
 public class ProductoRepository {
 
-    List<Producto> productos=new ArrayList<>();
-
-    public Producto findProductoById(String id){
-        return productos.stream().filter(p->p.getId().equals(id)).findFirst().orElse(null);
-    }
-
-    public  List<Producto> getAllProductos() {
-        return productos;
-    }
-
-    public void addProducto(Producto producto){
-        productos.add(producto);
-    }
-
-    public void deleteProducto(Producto producto){
-        productos.remove(producto);
-    }
-
-
 }

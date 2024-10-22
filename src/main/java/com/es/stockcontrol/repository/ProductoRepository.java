618cf62b94e6e0bb57026d7d6c4a4e36cbfbd422
Abraham Rodriguez <arm.developer00@gmail.com>
2024-10-22 21:19:11 +0200

    Update ProductoRepository.java
    No está terminado.

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -1,10 +1,52 @@
 package com.es.stockcontrol.repository;
 
 import com.es.stockcontrol.model.Producto;
+import com.es.stockcontrol.utils.HibernateUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductoRepository {
+
+    public void getProducto(String idProducto) {
+        HibernateUtils.em.getTransaction().begin();
+        HibernateUtils.em.getTransaction().commit();
+    }
+
+    public List<Producto> getProductosConStock() {
+        HibernateUtils.em.getTransaction().begin();
+        List<Producto> productos = HibernateUtils.em.createQuery("SELECT * FROM Producto WHERE stock > 0", Producto.class).getResultList();
+        HibernateUtils.em.getTransaction().commit();
+        return productos;
+    }
+
+    public List<Producto> getProductosSinStock() {
+        HibernateUtils.em.getTransaction().begin();
+        List<Producto> productos = HibernateUtils.em.createQuery("SELECT * FROM Producto WHERE stock = 0", Producto.class).getResultList();
+        HibernateUtils.em.getTransaction().commit();
+        return productos;
+    }
+
+    public void altaProducto(String idProducto, String nombreProducto, String precioSinIva, String descripcionProducto,String idProveedor, String nombreProveedor, String direccionProveedor) {
+        HibernateUtils.em.getTransaction().begin();
+
+        HibernateUtils.em.getTransaction().commit();
+    }
+
+    public void bajaProducto(String idProducto) {
+        HibernateUtils.em.getTransaction().begin();
+        HibernateUtils.em.remove(HibernateUtils.em.find(Producto.class, idProducto));
+        HibernateUtils.em.getTransaction().commit();
+    }
+
+    public void modificarProducto(String idProducto, String nuevoNombre) {
+        HibernateUtils.em.getTransaction().begin();
+        HibernateUtils.em.getTransaction().commit();
+    }
+
+    public void modificarStock(String idProducto, String nuevoStock) {
+        HibernateUtils.em.getTransaction().begin();
+        HibernateUtils.em.getTransaction().commit();
+    }
 
 }

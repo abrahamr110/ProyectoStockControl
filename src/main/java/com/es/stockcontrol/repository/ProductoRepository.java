64dd24429ac9c9d42b98123319106f5b27808f1c
Abraham Rodriguez <arm.developer00@gmail.com>
2024-10-24 16:50:09 +0200

    Finis ProductoRepository.java

--- src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
+++ src/main/java/com/es/stockcontrol/repository/ProductoRepository.java
@@ -1,6 +1,7 @@
 package com.es.stockcontrol.repository;
 
 import com.es.stockcontrol.model.Producto;
+import com.es.stockcontrol.model.Proveedor;
 import com.es.stockcontrol.utils.HibernateUtils;
 
 import java.util.List;
@@ -37,6 +38,21 @@ public class ProductoRepository {
 
     public void altaProducto(String idProducto, String nombreProducto, String precioSinIva, String descripcionProducto,String idProveedor, String nombreProveedor, String direccionProveedor) {
         HibernateUtils.em.getTransaction().begin();
+        Producto producto=new Producto();
+        Proveedor proveedor=new Proveedor();
+
+        producto.setId(idProducto);
+        producto.setNombre(nombreProducto);
+        producto.setPrecio_sin_iva(Float.parseFloat(precioSinIva));
+        producto.setDescripcion(descripcionProducto);
+
+        proveedor.setId(Long.parseLong(idProveedor));
+        proveedor.setNombre(nombreProveedor);
+        proveedor.setDireccion(direccionProveedor);
+
+        producto.setProveedor(proveedor);
+
+        HibernateUtils.em.persist(producto);
 
         HibernateUtils.em.getTransaction().commit();
     }
@@ -49,11 +65,23 @@ public class ProductoRepository {
 
     public void modificarProducto(String idProducto, String nuevoNombre) {
         HibernateUtils.em.getTransaction().begin();
+
+        HibernateUtils.em.createQuery("UPDATE Producto SET nombre = :nuevoNombre WHERE id = :id")
+                .setParameter("nuevoNombre", nuevoNombre)
+                .setParameter("id", idProducto)
+                .executeUpdate();
+
         HibernateUtils.em.getTransaction().commit();
     }
 
     public void modificarStock(String idProducto, String nuevoStock) {
         HibernateUtils.em.getTransaction().begin();
+
+        HibernateUtils.em.createQuery("UPDATE Producto SET stock = :nuevoStock WHERE id = :id")
+                        .setParameter("nuevoStock", nuevoStock)
+                        .setParameter("id", idProducto)
+                        .executeUpdate();
+
         HibernateUtils.em.getTransaction().commit();
     }
 
